17ac84ba99d4ff80b41ddfbfb625e57b96f4abff
Merce Pereira <merce.pereira.cao@gmail.com>
2016-11-28 19:37:44 +0100

    added middleware pattern

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars')
 
 const app = express()
 
+// view definitions
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
@@ -14,10 +15,29 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')
 app.set('views',path.join(__dirname,'views'))
 
+// middleware pattern
+
+app.use((request, response, next) => {
+  console.log(request.headers)
+  next()
+})
+
+app.use((request, response, next) => {
+  request.chance = Math.random()
+  next()
+})
+
 app.get('/', (request, response) => {  
+  //throw 44
   response.render('home', {
-    name: 'John'
-  })
+    name: 'John' + request.chance
+ })
+})
+
+app.use((err, request, response, next) => {  
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
 })
 
 app.listen(3000)
